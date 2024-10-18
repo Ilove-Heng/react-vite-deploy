83f6bc03a378626d02913c1d6bc6096c3afa2583
Nang SeakHeng <seakhengclever@gmail.com>
2024-10-18 15:00:12 +0700

    update test

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
       <h1>Vite cool robot + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count cool is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
